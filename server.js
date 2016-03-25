e97081334f3b69b9c4622355878dbfc4f9ba6ae9
ccfromstar <114413511@qq.com>
2016-03-25 14:57:02 +0800

    update 2016-3-25

--- server.js
+++ server.js
@@ -19,6 +19,22 @@ io.on('connection',function(socket){
             socket.emit('success');
             io.sockets.emit('system', data,users.length,'login'); //向所有连接到服务器的客户端发送当前登陆用户的昵称 
         };	
-	})
+	});
+	//断开连接的事件
+	socket.on('disconnect', function() {
+		if(socket.nickname){
+			users.splice(socket.userIndex, 1);
+	    	//通知除自己以外的所有人
+	    	socket.broadcast.emit('system', socket.nickname, users.length, 'logout');
+		}
+	});
+	socket.on('postMsg', function(msg) {
+        //将消息发送到除自己外的所有用户
+        socket.broadcast.emit('newMsg', socket.nickname, msg);
+    });
+    socket.on('img', function(imgData) {
+	    //通过一个newImg事件分发到除自己外的每个用户
+	    socket.broadcast.emit('newImg', socket.nickname, imgData);
+	});
 });
 
